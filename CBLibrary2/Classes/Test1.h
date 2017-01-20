6939cca088aee1a1852555118d3ec70eef5225a6
com3611923 <caobincool@163.com>
2017-01-20 16:35:43 +0800

    add test1

---
+++ CBLibrary2/Classes/Test1.h
@@ -0,0 +1,15 @@
+//
+//  Test1.h
+//  Pods
+//
+//  Created by caobin on 17/1/20.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test1 : NSObject
+
+- (void)test1;
+
+@end
---
+++ CBLibrary2/Classes/Test1.m
@@ -0,0 +1,18 @@
+//
+//  Test1.m
+//  Pods
+//
+//  Created by caobin on 17/1/20.
+//
+//
+
+#import "Test1.h"
+
+@implementation Test1
+
+- (void)test1
+{
+    NSLog(@"test1....");
+}
+
+@end
